641e19d6134fb5d74b33790d4634053ead3a186e
Zlobryak <clychnik@gmail.com>
2025-05-27 04:26:26 +0300

    solved on test data

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,21 @@ import ru.ntology.service.LeapYearCalc;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int successCount = 0;
 
-        System.out.println("Введите год в формате: yyyy");
-        int year = scanner.nextInt();
-        System.out.println("Введите количество дней: yyyy");
-        int numberOfDays = scanner.nextInt();
-        System.out.println("В этом году " + LeapYearCalc.leapYearCalc(year) + "дней");
+        while (true) {
+            System.out.println("Введите год в формате: yyyy");
+            int year = scanner.nextInt();
+            System.out.println("Введите количество дней: yyyy");
+            int numberOfDays = scanner.nextInt();
+
+            if (LeapYearCalc.leapYearCalc(year) == numberOfDays){
+                successCount++;
+            } else {
+                System.out.println("Неправильно! В этом году" + LeapYearCalc.leapYearCalc(year));
+                System.out.println(successCount);
+                break;
+            }
+        }
     }
 }
